5009f3d1e495168eadf07b5daf03c61da9f7b33e
pengunix <hoshimaemi@outlook.com>
2023-11-27 15:09:40 +0800

    test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <iostream>
+#include <opencv2/highgui/highgui.hpp>
 #include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -12,6 +13,11 @@
 #include <sys/prctl.h>
 #include <time.h>
 #include <unistd.h>
+
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 #include "sample_comm.h"
 
@@ -72,13 +78,39 @@ int main() {
 
   s32Ret = SAMPLE_COMM_VI_GetChnFrame(&ctx->vi, &pData);
   // pData is image yuv420sp
-  std::cout << ctx->vi.stViFrame.stVFrame.pMbBlk << std::endl;
-  FILE *fp = fopen("./frame.bin", "wb");
-  fwrite(pData, 1, ctx->vi.stViFrame.stVFrame.u64PrivateData, fp);
-  fclose(fp);
+  // std::cout << ctx->vi.stViFrame.stVFrame.pMbBlk << std::endl;
+  // FILE *fp = fopen("./frame.bin", "wb");
+  // fwrite(pData, 1, ctx->vi.stViFrame.stVFrame.u64PrivateData, fp);
+  // fclose(fp);
 
+  // cv::Mat srcMat(ctx->vi.u32Height, ctx->vi.u32Width, CV_8UC3, pData);
+
+  // cv::Mat outMat;
+  // cv::cvtColor(srcMat, outMat, cv::COLOR_YUV2BGR_NV12);
+
+  // cv::imwrite("A.png", outMat);
   // vi_get_stream(&ctx->vi);
   // Destroy VI
+
+  cv::Mat yuv;
+  cv::Mat m_color;
+
+  std::cout << ctx->vi.stViFrame.stVFrame.u64PrivateData << std::endl;
+  std::cout << ctx->vi.u32Height << std::endl;
+  std::cout << ctx->vi.u32Width << std::endl;
+
+
+  yuv.create(ctx->vi.u32Height * 3 / 2, ctx->vi.u32Width, CV_8UC1);
+  std::cout << "yuv create done" << std::endl;
+  memcpy(yuv.data, pData, ctx->vi.u32Height * ctx->vi.u32Width * 3 / 2);
+  std::cout << "yuv copy done" << std::endl;
+  free(pData);
+  std::cout << "free done" << std::endl;
+  cv::cvtColor(yuv, m_color, cv::COLOR_YUV2RGB_I420);
+  std::cout << "cvtColor done" << std::endl;
+  yuv.deallocate();
+  cv::imwrite("./output.png", m_color);
+
   SAMPLE_COMM_VI_DestroyChn(&ctx->vi);
 
   RK_MPI_SYS_Exit();
